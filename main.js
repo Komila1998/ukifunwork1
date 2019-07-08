9e033a617ddd7a831c623b224fd1da2f68ade537
kkomi23 <komikrish02@gmail.com>
2019-07-08 12:24:24 +0530

    funwork

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ $(document).ready(function() {
                });
 });
 });
-//ngk
 
+//kanavilaje netrodu
 $(document).ready(function() {
- SC.stream('/tracks/615375408',function(sound){
+ SC.stream('/tracks/462792732',function(sound){
            $("#start1").click(function(e) {
              e.preventDefault();
              sound.start();
@@ -33,10 +33,9 @@ $(document).ready(function() {
 });
 });
 
-
-//devid
+//pooraaaleeee ponnuthajeee
 $(document).ready(function() {
- SC.stream('/tracks/112010796',function(sound){
+ SC.stream('/tracks/510823947',function(sound){
            $("#start2").click(function(e) {
              e.preventDefault();
              sound.start();
@@ -47,14 +46,114 @@ $(document).ready(function() {
               });
 });
 });
-//
+//bombay hamma hamma
 $(document).ready(function() {
- SC.stream('/tracks/246940098',function(sound){
+ SC.stream('/tracks/390797712',function(sound){
            $("#start3").click(function(e) {
              e.preventDefault();
              sound.start();
             });
              $("#stop3").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});
+//rhythm
+$(document).ready(function() {
+ SC.stream('/tracks/364718444',function(sound){
+           $("#start4").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop4").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});//kadal
+$(document).ready(function() {
+ SC.stream('/tracks/66498404',function(sound){
+           $("#start5").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop5").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});
+//roja
+$(document).ready(function() {
+ SC.stream('/tracks/345472022',function(sound){
+           $("#start6").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop6").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});//kadhalan
+$(document).ready(function() {
+ SC.stream('/tracks/345528518',function(sound){
+           $("#start7").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop7").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});//kannalanee
+$(document).ready(function() {
+ SC.stream('/tracks/345495650',function(sound){
+           $("#start8").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop8").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});//nilla kaaikirathu
+$(document).ready(function() {
+ SC.stream('/tracks/347768843',function(sound){
+           $("#start9").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop9").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});
+//muthal murai
+$(document).ready(function() {
+ SC.stream('/tracks/572689890',function(sound){
+           $("#start10").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop10").click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+});
+});
+//santhana thenral
+$(document).ready(function() {
+ SC.stream('/tracks/517722948',function(sound){
+           $("#start11").click(function(e) {
+             e.preventDefault();
+             sound.start();
+            });
+             $("#stop11").click(function(e) {
                 e.preventDefault();
                 sound.stop();
               });
